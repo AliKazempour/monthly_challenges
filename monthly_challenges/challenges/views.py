e7bc3d7f7e39d7b66fb69158cafbb2319c6f9eea
AliKazempour <Alikazempoor83@gmail.com>
2023-09-25 22:16:33 +0330

    ✨ feat : add monthly_challenges dict for better style of code

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,14 +3,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
+monthly_challenges = {
+    "january": "Go to university very early ",
+    "february": "Playing video game every day",
+    "march": "Reading a Book",
+    "april": "Learn a New Skill Challenge",
+    "may": "30-Day Digital Detox Challenge",
+    "june": "30-Day Fitness Challenge",
+    "july": "Random Acts of Kindness Challenge",
+    "august":  "Journaling Challenge",
+    "september":  "Photography Challenge",
+    "october": "30-Day Meditation Challenge",
+    "november": "Gratitude Challenge",
+    "december": "The Giving Challenge"
+}
+
+
 def month_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Go to university very early "
-    elif month == "february":
-        challenge_text = "Playing video game every day"
-    elif month == "march":
-        challenge_text = "Reading a Book"
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This month is not exist!")
-    return HttpResponse(challenge_text)
