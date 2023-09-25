6dc85bfac31c5ea97becc9b5b89897dd4d7ddd73
AliKazempour <Alikazempoor83@gmail.com>
2023-09-25 21:12:42 +0330

    ✨ feat : add january challenge

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def january(request):
+    return HttpResponse("Go to university very early ")
+
