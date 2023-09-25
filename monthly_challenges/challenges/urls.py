e84017bd17804aeb11a4da583d70db37daaf65bb
AliKazempour <Alikazempoor83@gmail.com>
2023-09-25 21:33:58 +0330

    🎨 style : improve format of code (adding month_challenge method)

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('january/', views.january),
-    path('February/',views.february)
+    path('<month>/',views.month_challenge),
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
 
-
-def january(request):
-    return HttpResponse("Go to university very early ")
-
-def february(request):
-    return HttpResponse("Playing video game every day")
+def month_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Go to university very early "
+    elif month == "february":
+        challenge_text = "Playing video game every day"
+    elif month == "march":
+        challenge_text = "Reading a Book"
+    else:
+        return HttpResponseNotFound("This month is not exist!")
+    return HttpResponse(challenge_text)
