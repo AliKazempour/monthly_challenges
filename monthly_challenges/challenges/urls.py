36d5baf7c02d677d7bd5e66d4c327d8ddc85f23c
AliKazempour <Alikazempoor83@gmail.com>
2023-09-28 21:36:34 +0330

    ✨ feat : add index method for showing all months in a page

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('<int:month>/',views.monthly_challenge_by_number),
-    path('<str:month>/',views.month_challenge , name='month-challenge')
+    path("",views.index),
+    path("<int:month>",views.monthly_challenge_by_number),
+    path("<str:month>",views.month_challenge , name="month-challenge")
 ]
