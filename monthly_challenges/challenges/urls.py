e0d5a4b05b7d4a826d3b0eab1cdedbcba46f8625
AliKazempour <Alikazempoor83@gmail.com>
2023-09-28 20:25:03 +0330

    ⚡️ perf : improving performance of monthly_challenge_by_number method

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('<int:month>/',views.monthly_challenge_by_number),
-    path('<str:month>/',views.month_challenge)
+    path('<str:month>/',views.month_challenge , name='month-challenge')
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,7 @@
 from django.http import (HttpResponse, HttpResponseNotFound,
                          HttpResponseRedirect)
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
@@ -24,7 +25,8 @@ def monthly_challenge_by_number(request, month):
     if month > len(months) or month < 1:
         return HttpResponseNotFound("Invalid month!")
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge",args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 def month_challenge(request, month):
     try:
